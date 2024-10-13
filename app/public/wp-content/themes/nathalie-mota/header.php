cc30d401a16c3e3feafcf78df787442487573fe7
coralie montalbano <montalbanocoralie@gmail.com>
2024-10-13 14:30:53 +0200

    dernières améliorations

--- app/public/wp-content/themes/nathalie-mota/header.php
+++ app/public/wp-content/themes/nathalie-mota/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preload" href="<?php echo get_template_directory_uri(); ?>assets/fonts/SpaceMono-Regular.ttf" as="font" type="font/ttf" crossorigin>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
